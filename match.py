522ae427f95af8eef029eb3073f820249cc39a05
thebluescreenofdeath <torstenhkunz@googlemail.com>
2012-05-14 17:58:11 +0300

    Update match.py

--- match.py
+++ match.py
@@ -1,4 +1,4 @@
-# Fancy Matcher 2.3 - Sebastian Oehlschläger, Torsten Kunz
+# Fancy Matcher 2.4 - Sebastian Oehlschläger, Torsten Kunz
 
 import csv
 import sys
@@ -35,8 +35,9 @@ def create_searchword_list(csvfile):
      searchword_list = []
      for line in reader:
           split_phrase = []
-          split_phrase = [str.split(line[0])]
+          split_phrase = str.split(line[0])
           searchword_list.append([split_phrase, line[1]])
+#          print(searchword_list)
      return searchword_list
 
 def searchword_searchphrase_dict(csvfile):
@@ -46,6 +47,7 @@ def searchword_searchphrase_dict(csvfile):
      for line in reader:
           searchword_searchphrase_dict = {}
           split_searchphrase = str.split(line[0])
+
           for searchword in split_searchphrase:          
                searchword_searchphrase_dict.update({searchword:[]})
           input_list.append([searchword_searchphrase_dict, line[1]])
@@ -65,6 +67,7 @@ def vlookup_similar(lookup_list, lookup_dictionary):
                write_to_csv(file, header)
 
      counter = 0
+
      for searchphrase in lookup_list:
           counter = counter + 1
           if counter >= 100:
@@ -74,16 +77,20 @@ def vlookup_similar(lookup_list, lookup_dictionary):
                print(match_result/100)
                match_start = datetime.now()
 
-          for searchword in searchphrase[0]:
-               match_dbc = { "description":[0], "brand":[0], "category":[0] }
+          for searchword in searchphrase[0]:     
+
                for description, brand_category_sku in lookup_dictionary.items():
+                    match_dbc = { "description":[0], "brand":[0], "category":[0] }
                     split_name = []
                     split_name = str.split(description)
+
                     for partial_name in split_name:
                          name_match_ratio = difflib.SequenceMatcher(None, searchword, partial_name).ratio()
-                         if name_match_ratio > 0.90:
+                         if name_match_ratio > 0.85:
                               # Found SKU
+
                               for key in searchphrase[0]:
+
                                    split_category = []
                                    split_category = str.split(brand_category_sku[1])
                                    for partial_category in split_category:
@@ -92,22 +99,30 @@ def vlookup_similar(lookup_list, lookup_dictionary):
                                              if not key in match_dbc["category"][1:]:
                                                   match_dbc["category"][0] = 1
                                                   match_dbc["category"].append(key)
+
+
                                    split_brand = []
                                    split_brand = str.split(brand_category_sku[0])
+
                                    for partial_brand in split_brand:
                                         brand_match_ratio = difflib.SequenceMatcher(None, key, partial_brand).ratio()
                                         if brand_match_ratio > 0.75:
                                              if not key in match_dbc["category"][1:] and not key in match_dbc["brand"][1:]:
                                                   match_dbc["brand"][0] = 1
                                                   match_dbc["brand"].append(key)
+
                                    split_description = []
                                    split_description = str.split(description)
+                                   # desc_m_r_list = []
                                    for partial_description in split_description:
                                         description_match_ratio = difflib.SequenceMatcher(None, key, partial_description).ratio()
-                                        if description_match_ratio > 0.90:
+                                   #     desc_m_r_list.append(description_match_ratio)
+                                        if description_match_ratio > 0.85:
                                              if not key in match_dbc["category"][1:] and not key in match_dbc["brand"][1:] and not key in match_dbc["description"][1:]:
                                                   match_dbc["description"][0] = 1
-                                                  match_dbc["description"].append(key)
+                                                  match_dbc["description"].append(key+'-'+partial_description+'-'+str(description_match_ratio))
+                                        else:
+                                             pass
 
                               match_type = "empty"
                               if match_dbc["description"][0] == 1 and match_dbc["category"][0] == 1 and match_dbc["brand"][0] == 1:
@@ -123,7 +138,8 @@ def vlookup_similar(lookup_list, lookup_dictionary):
                               else:
                                    match_count = "incomplete match"
                               searched_words = []
-                              for word, empty in searchphrase[0].items():
+
+                              for word in searchphrase[0]:
                                    searched_words.append(word)
                               final_phrase = ' '.join(searched_words)
                               output = [[brand_category_sku[2], match_type, match_count, searchphrase[1], final_phrase, match_dbc, description, brand_category_sku[:2]]]
@@ -222,9 +238,7 @@ vlookup_similar(searchword_list, lookup_dict)
 #find_residuals(searchword_list)
 
 stop = datetime.now()
-print(stop)
 result = stop - start
 print(result)
-
 
 
